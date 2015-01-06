414b03db9fd2ac28ebdfbbd03634f16346885585
Michelle Yick <41213@longroad.ac.uk>
2015-01-06 19:11:30 +0000

    Still working on it

--- Class exercises. Development Q2.py
+++ Class exercises. Development Q2.py
@@ -1,5 +1,5 @@
 #Michelle Yick
-#02-01-2014
+#06-01-2015
 #Lists class exercises. Development Q2:
 
 #Get the random number from 0-10:
@@ -7,44 +7,42 @@ def random():
     import random 
     random_num=random.randint(0,10)
     return random_num
-#
+#Lists(capitals and the countries):
 def list1():
-    column1 = ["England","Australia","Germany","France","Lithuania","China","United States of America","Jamaica","Mexico","Thailand"]
+    column1=["England","Taiwan","Germany","France","Japan","China","United States of America","Spain","Mexico","South Korea"]
     return column1
 def list2 ():
-    column2 = ["London","Canberra","Berlin","Paris","Vilnius","Beijing","Washington","Kingston","Mexico City","Bangkok"]
+    column2=["London","Taipei","Berlin","Paris","Tokyo","Beijing","Washington","Madrid","Mexico City","Seoul"]
     return column2
 
-#cut the list
-def cut_list1 (column1,random_num):
-    country = column1[random_num]
+#Cut the list:
+def cut_list1(column1,random_num):
+    country=column1[random_num]
     return country
-#cut the list to get the capital
+#Cut the list to get the capital(slicing+list of lists):
 def cut_list2 (column2,random_num):
-    capital = column2[random_num]
+    capital=column2[random_num]
     return capital
-def ask_user (country,capital):
+def ask_user(country,capital):
 
 
-    answer = input("What is the caital {0}: ".format(country))
-    if answer == capital:
-        print("That is correct")
+    answer=input("What is the capital {0}: ".format(country))
+    if answer==capital:
+        print("Well Done. You guessed correctly. The capital city of {0} is {1}".format(country,capital))
     else:
-        print("That is wrong")
+        print("That is an incorrect guess")
 
+#Main program:
+count=0
+while count!=5:
+    count=count+1
+    answer=count
+print("You got {0} guesses right out of the 5 times".format(answer))
 
-
-
-
-#main program
-count = 0
-while count != 5:
-    count = count + 1
-
-    random_num = random()
-    column1 = list1()
-    column2 = list2()
-    country = cut_list1 (column1,random_num)
-    capital = cut_list2 (column2,random_num)
-    ask_user (country,capital)
+random_num=random()
+column1=list1()
+column2=list2()
+country=cut_list1(column1,random_num)
+capital=cut_list2(column2,random_num)
+ask_user(country,capital)
 

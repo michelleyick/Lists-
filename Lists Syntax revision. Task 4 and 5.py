97732064486b5aaa1104a861f702076e7b1afc0c
41213 <41213@PC004141.coll.lr.local>
2015-01-07 14:23:30 +0000

    Lists syntax revision

---
+++ Lists Syntax revision. Task 4 and 5.py
@@ -0,0 +1,15 @@
+#Michelle Yick
+#07-01-2015
+#Lists syntax revision. Task 4/5:
+
+pets=["dog","cat","goldfish","rabbit","gerbil"]
+
+#Task 4
+count=0
+for each in pets:
+    count=count+1
+    print("{0}. {1}".format(count,each))
+    
+#Task 5
+for each in pets:
+		print(each)

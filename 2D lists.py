dac9b2c30c8f369c6e87795020ebdabc83a345e3
Michelle Yick <41213@longroad.ac.uk>
2015-01-09 19:33:10 +0000

    Still working on it

--- 2D lists.py
+++ 2D lists.py
@@ -2,9 +2,22 @@
 #07-01-2015
 #2D Lists
 
-#The different rows:
-K1llmAchine=[51,49]
-bob2247=[5,99]
-hAxOr12=[72,30]
+def different_player_info():
+    #The different rows(3 different lists):
+    K1llmAchine=[51,49]
+    bob2247=[5,99]
+    hAxOr12=[72,30]
+    #Storing 3 lists in 1 list
+    data=[K1llmAchine,bob2247,hAxOr12]
+    return data
 
-data=[K1llmAchine,bob2247,hAxOr12]
+def data_wanted(data):
+    wanted_data=int(input("Enter 
+
+
+
+
+
+
+
+
